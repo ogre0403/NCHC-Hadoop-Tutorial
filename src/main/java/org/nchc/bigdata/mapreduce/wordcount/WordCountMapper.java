c01af623e1d00ea7bc97eace4c854bc1c7e1b436
Chia-Chuan Chuang <ogre0403@gmail.com>
2014-09-26 10:50:50 +0800

    add log facility

--- src/main/java/org/nchc/bigdata/mapreduce/wordcount/WordCountMapper.java
+++ src/main/java/org/nchc/bigdata/mapreduce/wordcount/WordCountMapper.java
@@ -5,21 +5,26 @@ import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
-
-
+import org.apache.log4j.Logger;
 
 
 public  class WordCountMapper extends Mapper<Object, Text, Text, IntWritable>{
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
-    
+    private static Logger logger = Logger.getLogger(WordCountMapper.class);
     @Override
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      
+
+      logger.info("input sentence " + value.toString());
       StringTokenizer itr = new StringTokenizer(value.toString());
+      int i = 0;
       while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
+        i++;
+        String _word;
+        _word = itr.nextToken();
+        logger.info( i + "_th world in input sentence is [" + _word + "]");
+        word.set(_word);
         context.write(word, one);
       }
     }
